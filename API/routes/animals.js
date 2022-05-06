83af56003dcd1c63a187cb51f131e1bee17f0253
shibisuriya@facilio.com <shibisuriya.work@gmail.com>
2022-05-06 18:45:31 +0530

    Update cron job

--- API/routes/animals.js
+++ API/routes/animals.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 router.get("/animals", async (req, res) => {
   const animals = [{ name: "Zebra" }, { name: "Dog" }, { name: "Bear" }];
-  res.status(200).json(animals);
+  const resp = {
+    // "EC2 timer": res.ec2_timer,
+    // animals: animals,
+    date_components: res.date_components,
+  };
+  console.log(resp);
+  res.status(200).json(resp);
 });
 module.exports = router;
--- API/updateCron.js
+++ API/updateCron.js
@@ -1,5 +1,41 @@
+var moment = require("moment");
 module.exports = function updateCron(req, res, next) {
-  var date_ob = new Date();
-  console.log("Update cron here... ", date_ob);
+  const date = moment().utc();
+  const DELAY_IN_MINUTES = 1440;
+  console.log("current date = ", date.format("hh:mm:ss a"));
+  // Date is getting mutated by .add() method.
+  date.add(DELAY_IN_MINUTES, "minutes");
+  res.ec2_timer =
+    "EC2 will shutdown at " +
+    date.format("hh:mm:ss a") +
+    " after " +
+    DELAY_IN_MINUTES +
+    " minutes delay...";
+  const minutes = date.format("mm");
+  const hour = String(date.hours());
+  const day_of_month = date.format("DD");
+  const month = date.format("MM");
+  const year = date.year();
+  cron_express =
+    "cron( " +
+    +minutes +
+    " " +
+    +hour +
+    " " +
+    +day_of_month +
+    " " +
+    +month +
+    " ? " +
+    +year +
+    " )";
+  res.date_components = {
+    // minutes: minutes,
+    // hour: hour,
+    // day_of_month: day_of_month,
+    // month: month,
+    // year: year,
+    date: date.format("DD-MM-yyyy hh:mm:ss a"),
+    cron_express: cron_express,
+  };
   next();
 };
