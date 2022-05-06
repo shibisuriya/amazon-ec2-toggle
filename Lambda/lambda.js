6013f84211d4a271c9c462a4eb36e8a069363a64
shibisuriya@facilio.com <shibisuriya.work@gmail.com>
2022-05-07 00:07:30 +0530

    Added notes

--- Lambda/lambda.js
+++ Lambda/lambda.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.get("/hello", async (req, res) => {
+  // Check if the EC2 instance is down, if it is down then bring it back up, else do nothing.
   // Code to up the EC2 instance....
 
+  // Check if the EC2 instance is up, if it is up then shut it down, if it is already down then do nothing.
   // Code to shutdown the EC3 instance...
 
   const animals = [{ name: "Zebra" }, { name: "Dog" }, { name: "Bear" }];
