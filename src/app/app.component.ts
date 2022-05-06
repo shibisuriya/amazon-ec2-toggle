13cdb962a45c3d1b30850a2fe0d6d420c17ff3a4
shibisuriya@facilio.com <shibisuriya.work@gmail.com>
2022-05-06 19:31:44 +0530

    Added CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,21 @@ export class AppComponent {
   constructor(private animalService: AnimalsService) {
 
   }
-  data = null
+  data: any = null
+  cron_expression = null;
+  time_components: any = null;
+  EC2_timer: any = null;
+  time: any = null
   ngOnInit() {
   }
   getAnimals() {
     this.animalService.get()
       .subscribe((resp: any) => {
         this.data = resp.middle_ware_data
+        this.EC2_timer = this.data.EC2_timer
+        this.cron_expression = this.data.cron_expression
+        this.time_components = this.data.time_components
+        this.time = this.data.time
 
       })
   }
